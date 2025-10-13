17d55bd869404146a634e7f96b4169e38a30711a
Charles Osheokwu <chasseze@gmail.com>
2025-10-13 02:27:36 -0500

    Bind server to 0.0.0.0 for Railway deployment

--- server.js
+++ server.js
@@ -531,8 +531,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
     if (useLibsql) {
         console.log(`Using LibSQL database: ${process.env.LIBSQL_URL}`);
     } else {
